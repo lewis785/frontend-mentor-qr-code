cd2ab73e963cb60b5c5db5b5e74ae85c36b5b699
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2023-02-03 23:49:02 +0000

    ADD host urls

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/frontend-mentor-qr-code/",
   plugins: [react()],
   css: {
     modules: {
