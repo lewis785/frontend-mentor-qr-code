aed5943411115ce4c33aa049df95e6430047f8ec
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2023-02-04 00:07:44 +0000

    FIX deployment problems

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/frontend-mentor-qr-code/",
+  base: "frontend-mentor-qr-code",
   plugins: [react()],
   css: {
     modules: {
